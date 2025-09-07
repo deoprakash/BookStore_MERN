6048c314cb83546973c8842f1466b0fd1276bfff
DEO PRAKASH <deo.prakash@ssipmt.com>
2025-09-07 23:18:06 +0530

    Some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Banner from "./components/Banner";
+import CartPage from "./pages/CartPage";
+import AboutPage from "./pages/AboutPage";
+import BookPage from "./pages/BookPage";
+import ContactPage from "./pages/ContactPage";
+import CartPage from "./pages/CartPage";
+import Login from "./components/Login";
+import Signup from "./assets/dummystyles";
 
 const App = () => {
   return (
     <Routes>
       <Route path = '/' element = {<Home />} />
+      <Route path = '/cart' element = {<CartPage />} />
+      <Route path = '/about' element = {<AboutPage />} />
+      <Route path = '/books' element = {<BookPage />} />
+      <Route path = '/books' element = {<ContactPage />} />
+
+      <Route path = '/login' element = {<Login />} />
+      <Route path = '/signup' element = {<Signup />} />
+
+
       
     </Routes>
   )
---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Signup } from '../assets/dummystyles'
+
+const Signup = () => {
+  return (
+    <div>Signup </div>
+  )
+}
+
+export default Signup
