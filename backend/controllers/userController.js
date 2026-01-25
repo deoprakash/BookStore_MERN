ec7872c92f9fb9a337baa6f5c43eeee26b818028
DEO PRAKASH <deo.prakash@ssipmt.com>
2026-01-25 16:44:14 +0530

    Designed Login and Registration Route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,19 @@
-import userModel from "../models/UserModel";
+import userModel from "../models/UserModel.js";
 import validator from 'validator'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_token';
+const TOKEN_EXPIRES = '24h';
+
+// TOKEN GENERATION
+
+const createToken = (userId) => 
+    jwt.sign({id: userId}, JWT_SECRET, {expiresIn: TOKEN_EXPIRES });
 
 // REGISTER FUNCTION
 
-export async function registerUser(req, req) {
+export async function registerUser(req, res) {
     const {username, email, password} = req.body;
     
     if (!username || !email || !password){
@@ -20,7 +30,97 @@ export async function registerUser(req, req) {
     if (password.length < 8){
         return res.status(400).json({
             success: false,
-            message: 'Password must be atealst 8 characters.'
+            message: 'Password must be atleast 8 characters.'
         })
+    }
+
+    try {
+        const userExists = await userModel.findOne({ email })
+        if (userExists) {
+            return res.status(409).json({
+                success: false,
+                message: 'User already exists.'
+            })
+        }
+        const hashedPassword =  await bcrypt.hash(password, 10) //salt value is 10 means how many times does a password should be hashed.
+
+        const user = await userModel.create({
+            name: username,
+            email,
+            password: hashedPassword,
+        })
+        const token = createToken(user._id);
+
+        res.status(201).json({
+            success: true,
+            message: 'Acound Created Successfully',
+            token,
+            user: {
+                id: user._id,
+                username: user.name,
+                email: user.email,
+            }
+        })
+    }
+
+    catch (error) {
+        console.error('Register error:', error);
+        res.status(500).json({
+            success: false,
+            message: 'Server error.'
+        })
+        
+    }
+}
+
+// LOGIN FUNCTION
+
+export async function loginUser (req, res) {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: 'All fields are required.'
+        })
+    }
+
+    try {
+        const user = await userModel.findOne({email});
+        if (!user) {
+            return res.status(401).json({ 
+                success: false,
+                message: "Invalid email or password"
+            })
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid email or password.'
+            })
+        }
+
+        const token = createToken(user._id);
+
+        res.status(200).json({
+            success: true,
+            message: 'Login Successfull!',
+            token,
+            user: {
+                id: user._id,
+                username: user.name,
+                email: user.email,
+            }
+        })
+    }
+
+    catch (error) {
+        console.error('Login error:', error);
+        res.status(500).json({
+            success: false,
+            message: 'Server error.'
+        })
+        
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { connectDB } from './config/db.js';
+import userRouter from './routes/userRoute.js';
 
 
 
@@ -23,7 +24,7 @@ connectDB();
 
 // ROUTES
 
-
+app.use('/api/user', userRouter);
 
 app.get('/', (req, res) => {
     res.send('API WORKING')
